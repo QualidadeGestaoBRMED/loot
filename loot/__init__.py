82415c093cf31e525a60dd5a438dd117b430a5d8
rdsgabriel <rdsgabriel.dev@gmail.com>
2025-11-26 15:38:05 -0300

    agora vai

---
+++ loot/__init__.py
@@ -0,0 +1,11 @@
+"""
+Loot - Repositorio central de ativos de automacao e bibliotecas compartilhadas.
+
+Steal like an artist :)
+"""
+
+__version__ = "0.1.0"
+
+from . import parsers
+
+__all__ = ["parsers"]
---
+++ loot/parsers/__init__.py
@@ -0,0 +1,27 @@
+"""
+Modulo de parsers e validadores de dados.
+"""
+
+from .id_validators import (
+    DocType,
+    clean_digits,
+    clean_alphanumeric,
+    is_cpf_valid,
+    is_cnpj_valid,
+    is_passport_br_valid,
+    is_generic_passport_valid,
+    detect_doc_type,
+    process_document,
+)
+
+__all__ = [
+    "DocType",
+    "clean_digits",
+    "clean_alphanumeric",
+    "is_cpf_valid",
+    "is_cnpj_valid",
+    "is_passport_br_valid",
+    "is_generic_passport_valid",
+    "detect_doc_type",
+    "process_document",
+]
